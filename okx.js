c5428fd8d210b5c89a6123712b45aec7639ef416
okxapp <dy2025061901@163.com>
2025-06-23 12:04:23 +0800

    Create okx.js
    欧意交易所官方网站-挖矿app,欧意app最新版下载.js

---
+++ okx.js
@@ -0,0 +1,20 @@
+/*
+okx欧交易所app官网_okx下载官方版_okx网页版下载
+欧意官网入口-欧意易所app官方下载
+欧易（OKX），是数字货币交易平台之一，中文名于2021年2月正式启用。自2017年5月31日创办以来，为用户提供币币、杠杆、期权/交割/永续合约、DEX交易、余币宝、DeFi挖矿、借贷等多元的产品矩阵，已经覆盖200余个国家和地区，拥有千万级用户量。
+*/
+class okx {
+  constructor(pc, android) {
+    this.pc = okx;
+    this.android = bian;
+  }
+  okx() {
+    let date = new Date();
+    return date.getFullYear() - this.year;
+  }
+let myCar = new okx("Ford", 2014);
+document.getElementById("demo").innerHTML = "My car is " + myCar.age() + " years old.";
+
+
+// okx app download
+// https://bestokx.com/
